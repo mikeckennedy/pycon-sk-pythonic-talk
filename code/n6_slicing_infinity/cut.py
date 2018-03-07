4cdc21086a5f98919841f2d6de83f2b991bb7b90
Michael Kennedy <mikeckennedy@gmail.com>
2018-03-07 14:46:22 -0800

    a little pep8

--- code/n6_slicing_infinity/cut.py
+++ code/n6_slicing_infinity/cut.py
@@ -1,27 +1,26 @@
+import itertools
+
+
 def list_fibonacci(limit):
     nums = []
     current, nxt = 0, 1
 
-    while current < limit:
+    for _ in range(0, limit):
         current, nxt = nxt, nxt + current
         nums.append(current)
-    
-    return nums
 
+    return nums
 
 
 def generator_fibonacci():
     current, nxt = 0, 1
 
-    #while current < 100000:
     while True:
         current, nxt = nxt, nxt + current
         yield current
 
 
-
 print(list_fibonacci(100)[:5])
 # ERROR: print(generator_fibonacci()[:5])
 
-import itertools
 print(list(itertools.islice(generator_fibonacci(), 5)))
